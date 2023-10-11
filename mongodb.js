5586cec814c6d2224b999701ed4638fbc2f07c7a
Parv-gugnani <9765.parv@gmail.com>
2023-10-11 14:16:32 +0530

    done

---
+++ mongodb.js
@@ -0,0 +1,25 @@
+// Crud Operations
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-app";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    //
+    if (error) {
+      return console.log("Unable to connect to database!");
+    }
+    console.log("Connected!!!!!");
+
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne({
+      name: "Parv",
+      age: 18,
+    });
+  }
+);
